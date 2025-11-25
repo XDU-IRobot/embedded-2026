ca350f56c1ff01d9ae7ab7cd952a49f66da59c33
FHb212 <161186518+FHb212@users.noreply.github.com>
2025-11-25 14:10:21 +0000

    Fix code style issues with clang-format

--- app/targets/dart_rack/main.cc
+++ app/targets/dart_rack/main.cc
@@ -4,19 +4,16 @@
 #include "tim.h"
 #include "timer_task.hpp"
 
-
-void MainLoop(){
-
-}
+void MainLoop() {}
 extern "C" [[noreturn]] void AppMain(void) {
   dart_rack = new DartRack();
   DartRack::Init();
   // 创建主循环定时任务，定频1khz
   TimerTask mainloop_1000hz{
-    &htim13,                                   //
-    etl::delegate<void()>::create<MainLoop>()  //
-};
- // mainloop_1000hz.SetPrescalerAndPeriod(168 - 1, 1000 - 1);  // 84MHz / 168 / 1000 = 500Hz
+      &htim13,                                   //
+      etl::delegate<void()>::create<MainLoop>()  //
+  };
+  // mainloop_1000hz.SetPrescalerAndPeriod(168 - 1, 1000 - 1);  // 84MHz / 168 / 1000 = 500Hz
   mainloop_1000hz.Start();
 
   for (;;) {
