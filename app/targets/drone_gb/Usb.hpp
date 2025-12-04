7780cf83ceb47616eb5acda576d66288a4f4d342
Zyf-de-github <180545991+Zyf-de-github@users.noreply.github.com>
2025-12-04 09:39:18 +0000

    Fix code style issues with clang-format

--- app/targets/drone_gb/Usb.hpp
+++ app/targets/drone_gb/Usb.hpp
@@ -7,29 +7,29 @@
 #define AIMBOT_DATA_RECEIVE_ID 0x2  // 接收数据帧ID 0x2
 
 typedef struct __attribute__((packed)) {
-  uint8_t _SOF;            // 包头
-  uint8_t ID;              // 接收id
-  uint8_t AimbotState;     // 自瞄状态
-  uint8_t AimbotTarget;    // 自瞄目标
-  float PitchRelativeAngle;// pitch角度值
-  float YawRelativeAngle;  // yaw角度值
-  float TargetPitchSpeed;  // 目标pitch速度
-  float TargetYawSpeed;    // 目标yaw速度
-  uint32_t SystemTimer;    // 系统时间
-  uint8_t _EOF;            // 包尾
+  uint8_t _SOF;              // 包头
+  uint8_t ID;                // 接收id
+  uint8_t AimbotState;       // 自瞄状态
+  uint8_t AimbotTarget;      // 自瞄目标
+  float PitchRelativeAngle;  // pitch角度值
+  float YawRelativeAngle;    // yaw角度值
+  float TargetPitchSpeed;    // 目标pitch速度
+  float TargetYawSpeed;      // 目标yaw速度
+  uint32_t SystemTimer;      // 系统时间
+  uint8_t _EOF;              // 包尾
 } AimbotFrame_SCM_t;
 
 typedef struct __attribute__((packed)) {
-  uint8_t _SOF;      // 包头
-  uint8_t ID;        // 发送id
-  uint32_t TimeStamp;// 时间戳
-  float q0;          // 四元数
-  float q1;          //
-  float q2;          //
-  float q3;          //
-  uint8_t robot_id;  // 机器人id
-  uint8_t mode;      // 自瞄模式
-  uint8_t _EOF;      // 包尾
+  uint8_t _SOF;        // 包头
+  uint8_t ID;          // 发送id
+  uint32_t TimeStamp;  // 时间戳
+  float q0;            // 四元数
+  float q1;            //
+  float q2;            //
+  float q3;            //
+  uint8_t robot_id;    // 机器人id
+  uint8_t mode;        // 自瞄模式
+  uint8_t _EOF;        // 包尾
 } GimbalImuFrame_SCM_t;
 
 #ifdef __cplusplus
