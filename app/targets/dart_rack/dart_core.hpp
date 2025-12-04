ce0b44daa57ec9cfaba6b0bccca999d060db922c
Haonan Liu <messenger@lunariio.work>
2025-12-04 20:49:50 +0800

    dart_rack: 整理一下已有代码

--- app/targets/dart_rack/dart_core.hpp
+++ app/targets/dart_rack/dart_core.hpp
@@ -1,11 +1,10 @@
-#ifndef STATE_DEFINITIONS
-#define STATE_DEFINITIONS
+#pragma once
 
 #include <librm.hpp>
-#include "device_encoder.hpp"
-#include "dji_odometer.hpp"
-// 状态机变量定义
 
+#include "encoder_counter.hpp"
+
+// 状态机变量定义
 enum class AbleState : uint8_t { kOff = 0, kOn = 1 };
 
 enum class PhaseState : uint8_t { kUncomplete = 0, kDone = 1 };
@@ -29,6 +28,7 @@ struct ManualMode {
   PhaseState chamber = PhaseState::kUncomplete;
   PhaseState aim = PhaseState::kUncomplete;
   PhaseState fire = PhaseState::kUncomplete;
+
   void ManualModeClear()  // 清空所有标志位
   {
     mode = ModeState::kUnable;
@@ -59,46 +59,43 @@ enum class DartCount : uint8_t { kFirst = 0, kSecond = 1, kThird = 2, kFourth = 
 
 // 发射架核心结构体
 struct DartRack {
- public:
-  DartState *state{};
-  // 硬件接口指针
-  rm::hal::Can *can1{nullptr};     ///< CAN 总线接口
-  rm::hal::Serial *dbus{nullptr};  ///< 遥控器串口接口
-  rm::device::DR16 *rc{nullptr};   //< 遥控器
-  // can设备指针
-  rm::device::M3508 *load_motor_r{nullptr};         //< 右上膛电机
-  rm::device::M3508 *load_motor_l{nullptr};         //< 左上膛电机
-  rm::device::M2006 *trigger_motor{nullptr};        //< 扳机活动电机
-  rm::device::M2006 *trigger_motor_force{nullptr};  //< 扳机释放电机
-  rm::device::M2006 *yaw_motor{nullptr};            //< yaw轴调节电机
-                                                    // 编码器设备指针
-  rm::device::ME02 *yaw_encoder{nullptr};
-  // PID 控制器
-  rm::modules::PID *load_motor_r_speed_pid{};
-  rm::modules::PID *load_motor_l_speed_pid{};
-  rm::modules::PID *trigger_motor_speed_pid{};
-  rm::modules::PID *trigger_motor_force_pid{};
-  rm::modules::PID *yaw_motor_speed_pid{};
+  DartState state_{};
 
-  // DJI电机计圈器
-  rm::device::DjiOdometer *load_motor_r_odometer{nullptr};
-  rm::device::DjiOdometer *load_motor_l_odometer{nullptr};
-  rm::device::DjiOdometer *trigger_motor_odometer{nullptr};
-  rm::device::DjiOdometer *trigger_motor_force_odometer{nullptr};
-  DartCount dart_count = DartCount::kFirst;
+  // 硬件接口
+  rm::hal::Can *can1_{nullptr};     ///< CAN 总线接口
+  rm::hal::Serial *dbus_{nullptr};  ///< 遥控器串口接口
+
+  // 设备
+  rm::device::DR16 *rc_{nullptr};                    ///< 遥控器
+  rm::device::M3508 *load_motor_r_{nullptr};         ///< 右上膛电机
+  rm::device::M3508 *load_motor_l_{nullptr};         ///< 左上膛电机
+  rm::device::M2006 *trigger_motor_{nullptr};        ///< 扳机活动电机
+  rm::device::M2006 *trigger_motor_force_{nullptr};  ///< 扳机释放电机
+  rm::device::M2006 *yaw_motor_{nullptr};            ///< yaw轴调节电机
+  rm::device::JyMe02Can *yaw_encoder_{nullptr};      ///< 编码器
+
+  // PID 控制器
+  rm::modules::PID load_motor_r_speed_pid_{};
+  rm::modules::PID load_motor_l_speed_pid_{};
+  rm::modules::PID trigger_motor_speed_pid_{};
+  rm::modules::PID trigger_motor_force_pid_{};
+  rm::modules::PID yaw_motor_speed_pid_{};
+
+  // 编码器计圈器
+  EncoderCounter load_motor_r_odometer_;
+  EncoderCounter load_motor_l_odometer_;
+  EncoderCounter trigger_motor_odometer_;
+  EncoderCounter trigger_motor_force_odometer_;
+  DartCount dart_count_{DartCount::kFirst};
 
   // yaw轴相关常量
-  static constexpr float_t kYawEcdMax = 52.6000f;                      //< ME02 编码器最大值
-  static constexpr float_t kYawEcdMin = 35.5000f;                      //< ME02 编码器最小值
-  static constexpr float_t kYawEcd[4] = {47.0f, 44.0f, 48.0f, 50.0f};  //< ME02 编码器四发镖位置
-  static constexpr float_t kYawEcdSecond = 55.0f;
+  static constexpr float kYawEcdMax = 52.6000f;                      //< ME02 编码器最大值
+  static constexpr float kYawEcdMin = 35.5000f;                      //< ME02 编码器最小值
+  static constexpr float kYawEcd[4] = {47.0f, 44.0f, 48.0f, 50.0f};  //< ME02 编码器四发镖位置
+  static constexpr float kYawEcdSecond = 55.0f;
 
- private:
- public:
-  static void Init();
-  static void DataUpdate();
-  static void DataSend();
+  void Init();
+  void Update();
 };
 
 extern DartRack *dart_rack;
-#endif
