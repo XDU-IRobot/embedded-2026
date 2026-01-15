a7605d5596f96b0187c8123e0f03a18b0e8d5ede
Qijun Wang <2100921316@qq.com>
2026-01-15 22:03:43 +0800

    refactor：将main.cc的global和mainloop移到头文件

--- app/targets/meca_hero_cs/main.cc
+++ app/targets/meca_hero_cs/main.cc
@@ -1,52 +1,6 @@
 #include "main.hpp"
-#include "can.h"
-#include "usart.h"
-#include "spi.h"
 #include "timer_task.hpp"
 #include "tim.h"
-
-struct GlobalWarehouse {
-  // 硬件接口 //
-  rm::hal::Can *can1{nullptr}, *can2{nullptr};  ///< CAN 总线接口
-  rm::hal::Serial *dbus{nullptr};               ///< 遥控器串口接口
-
-  // 设备 //
-  rm::device::DR16 *rc{nullptr};                                                       ///< 遥控器
-  rm::device::GM6020 *yaw_motor{nullptr};                                              ///< 云台 Yaw 电机
-  rm::device::DmMotor<rm::device::DmMotorControlMode::kMit> *magazine_motor{nullptr};  ///< 云台 Pitch 电机
-  rm::device::BMI088 *imu{nullptr};                                                    ///< BMI088 IMU
-
-  // 控制器 //
-  rm::modules::MahonyAhrs ahrs{1000.f};  ///< mahony 姿态解算器，频率 1000Hz
-
-  void Init() {
-    can1 = new rm::hal::Can{hcan1};
-    can2 = new rm::hal::Can{hcan2};
-    dbus = new rm::hal::Serial{huart3, 36, rm::hal::stm32::UartMode::kNormal, rm::hal::stm32::UartMode::kDma};
-
-    rc = new rm::device::DR16{*dbus};  // 设置了遥控器以及用了串口
-    yaw_motor = new rm::device::GM6020{*can1, 1};
-    magazine_motor = new rm::device::DmMotor<rm::device::DmMotorControlMode::kMit>{*can2, {}};
-    imu = new rm::device::BMI088{hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port, CS1_GYRO_Pin};
-
-    can1->SetFilter(0, 0);
-    can1->Begin();
-    can2->SetFilter(0, 0);
-    can2->Begin();
-    rc->Begin();  // 启动遥控器接收，这行或许比较适合放到AppMain里面？
-  }
-} *globals;
-
-// 姿态解算任务
-void MainLoop() {
-  globals->imu->Update();
-  globals->ahrs.Update(rm::modules::ImuData6Dof{-globals->imu->gyro_y(),   //
-                                                -globals->imu->gyro_x(),   //
-                                                -globals->imu->gyro_z(),   //
-                                                -globals->imu->accel_y(),  //
-                                                -globals->imu->accel_x(),  //
-                                                -globals->imu->accel_z()});
-}
 
 extern "C" [[noreturn]] void AppMain(void) {
   /*启动CAn总线
