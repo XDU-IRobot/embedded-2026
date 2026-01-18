3d2f999e693942393dde120308857732b5f3c321
chipskiller <201592842+chipskiller@users.noreply.github.com>
2026-01-18 11:56:20 +0000

    Fix code style issues with clang-format

--- app/targets/meca_hero_cs/main.cc
+++ app/targets/meca_hero_cs/main.cc
@@ -7,18 +7,18 @@
 
 // 底盘速度
 rm::i16 Vx, Vy, Vw;
-//拨盘增加角度
+// 拨盘增加角度
 float target_magz;
 float target_velocity;
-//左摇杆状态
+// 左摇杆状态
 rm::device::DR16::SwitchPosition l_switch_position_now = rm::device::DR16::SwitchPosition::kUnknown;
 rm::device::DR16::SwitchPosition l_switch_position_last = rm::device::DR16::SwitchPosition::kUnknown;
 
-//扳机状态
-// enum class Trigger_state:rm::usize {
-//   kOff = 0u,
-//   kOn,
-// };
+// 扳机状态
+//  enum class Trigger_state:rm::usize {
+//    kOff = 0u,
+//    kOn,
+//  };
 
 // rm::usize trigger_now = 0u;
 // rm::usize trigger_last = 0u;
@@ -29,14 +29,14 @@ int counter = 0;
 
 struct GlobalWarehouse {
   // 硬件接口 //
-  rm::hal::Can *can1{nullptr}, *can2{nullptr}; ///< CAN 总线接口
-  rm::hal::Serial *dbus{nullptr}; ///< 遥控器串口接口
+  rm::hal::Can *can1{nullptr}, *can2{nullptr};  ///< CAN 总线接口
+  rm::hal::Serial *dbus{nullptr};               ///< 遥控器串口接口
 
   // 设备 //
-  rm::device::DR16 *rc{nullptr}; ///< 遥控器
+  rm::device::DR16 *rc{nullptr};  ///< 遥控器
   // rm::device::GM6020 *yaw_motor{nullptr};                                              ///< 云台 Yaw 电机
   // rm::device::DmMotor<rm::device::DmMotorControlMode::kMit> *magazine_motor{nullptr};  ///< 云台 Pitch 电机
-  rm::device::BMI088 *imu{nullptr}; ///< BMI088 IMU
+  rm::device::BMI088 *imu{nullptr};  ///< BMI088 IMU
 
   // 创建电机对象
   rm::device::M3508 *wheel_motor_1{nullptr};
@@ -57,14 +57,14 @@ struct GlobalWarehouse {
   rm::modules::PID *pid_magz_velocity{nullptr};
 
   // 控制器 //
-  rm::modules::MahonyAhrs ahrs{1000.f}; ///< mahony 姿态解算器，频率 1000Hz
+  rm::modules::MahonyAhrs ahrs{1000.f};  ///< mahony 姿态解算器，频率 1000Hz
 
   void Init() {
     can1 = new rm::hal::Can{hcan1};
     can2 = new rm::hal::Can{hcan2};
     dbus = new rm::hal::Serial{huart3, 36, rm::hal::stm32::UartMode::kNormal, rm::hal::stm32::UartMode::kDma};
     // 遥控
-    rc = new rm::device::DR16{*dbus}; // 设置了遥控器以及用了串口
+    rc = new rm::device::DR16{*dbus};  // 设置了遥控器以及用了串口
     // 电机
     wheel_motor_1 = new rm::device::M3508{*can1, 1, false};
     wheel_motor_2 = new rm::device::M3508{*can1, 2, false};
@@ -89,7 +89,7 @@ struct GlobalWarehouse {
     can1->Begin();
     can2->SetFilter(0, 0);
     can2->Begin();
-    rc->Begin(); // 启动遥控器接收，这行或许比较适合放到AppMain里面？
+    rc->Begin();  // 启动遥控器接收，这行或许比较适合放到AppMain里面？
   }
 } *globals;
 
@@ -142,22 +142,22 @@ void MainLoop() {
 
   rm::device::DjiMotor<>::SendCommand();
 
-  //拨盘电机逻辑
+  // 拨盘电机逻辑
   l_switch_position_last = l_switch_position_now;
   l_switch_position_now = globals->rc->switch_l();
   if (l_switch_position_now == rm::device::DR16::SwitchPosition::kMid) {
-    //初始化射击模式
+    // 初始化射击模式
     if (l_switch_position_last != rm::device::DR16::SwitchPosition::kMid) {
       globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kEnable);
       globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kSetZeroPosition);
       target_magz = 0.0f;
     }
-    //按下扳机(延时1s)
+    // 按下扳机(延时1s)
     if (counter == 0) {
       if (globals->rc->dial() >= 500 || globals->rc->dial() < -500) {
-        //增加60°
-        target_magz -= 1.0472/*（π/3）*/;
-        if (target_magz <= -3.141593/*（π）*/) {
+        // 增加60°
+        target_magz -= 1.0472 /*（π/3）*/;
+        if (target_magz <= -3.141593 /*（π）*/) {
           target_magz += 3.141593 * 2;
         }
         counter = 250;
@@ -165,15 +165,15 @@ void MainLoop() {
     } else {
       counter--;
     }
-    //拨盘电机反馈
-    pos = globals->magazine_motor->pos(); // rad
-    float vel = globals->magazine_motor->vel(); // rad/s
-    //拨盘电机串级PID（开循环）
+    // 拨盘电机反馈
+    pos = globals->magazine_motor->pos();        // rad
+    float vel = globals->magazine_motor->vel();  // rad/s
+    // 拨盘电机串级PID（开循环）
     globals->pid_magz_position->SetCircular(true).SetCircularCycle(3.141593 * 2);
     globals->pid_magz_position->Update(target_magz, pos, 0.002);
     target_velocity = globals->pid_magz_position->out();
     globals->pid_magz_velocity->Update(target_velocity, vel, 0.002);
-    //发送CAN
+    // 发送CAN
     if (globals->rc->switch_r() == rm::device::DR16::SwitchPosition::kDown) {
       globals->magazine_motor->SetPosition(0, 0, 0, 0, 0);
     } else {
@@ -181,9 +181,9 @@ void MainLoop() {
       globals->magazine_motor->SetPosition(0, 0, globals->pid_magz_velocity->out(), 0, 0);
     }
   }
-  //失能
-  if (l_switch_position_last == rm::device::DR16::SwitchPosition::kMid && l_switch_position_now !=
-      rm::device::DR16::SwitchPosition::kMid) {
+  // 失能
+  if (l_switch_position_last == rm::device::DR16::SwitchPosition::kMid &&
+      l_switch_position_now != rm::device::DR16::SwitchPosition::kMid) {
     globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
     globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
     globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
@@ -199,11 +199,11 @@ extern "C" [[noreturn]] void AppMain(void) {
 
   // 创建主循环定时任务，定频1khz
   TimerTask mainloop_1000hz{
-      &htim13, //
-      etl::delegate<void()>::create<MainLoop>() //
+      &htim13,                                   //
+      etl::delegate<void()>::create<MainLoop>()  //
   };
-  mainloop_1000hz.SetPrescalerAndPeriod(168, 1000 - 1); // 84MHz / 84 / 1000 = 1kHz
-  mainloop_1000hz.Start(); // 启动定时器
+  mainloop_1000hz.SetPrescalerAndPeriod(168, 1000 - 1);  // 84MHz / 84 / 1000 = 1kHz
+  mainloop_1000hz.Start();                               // 启动定时器
 
   for (;;) {
     __WFI();
