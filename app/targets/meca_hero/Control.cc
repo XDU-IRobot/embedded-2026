f14b9f85630f315db9f219a2a0de98d45acdc3f5
Qijun Wang <2100921316@qq.com>
2026-01-19 22:05:45 +0800

    fix：整理代码格式，将底盘电机控制转移到can2

---
+++ app/targets/meca_hero/Control.cc
@@ -0,0 +1,119 @@
+#include "main.hpp"
+
+void MagazineControl() {
+  //拨盘电机逻辑
+  l_switch_position_last = l_switch_position_now;
+  l_switch_position_now = globals->rc->switch_l();
+  if (l_switch_position_now == rm::device::DR16::SwitchPosition::kMid) {
+    //初始化射击模式
+    if (l_switch_position_last != rm::device::DR16::SwitchPosition::kMid) {
+      globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kEnable);
+      globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kSetZeroPosition);
+      target_magz = 0.0f;
+    }
+    //按下扳机(延时1s)
+    if (counter == 0) {
+      if (globals->rc->dial() >= 500 || globals->rc->dial() < -500) {
+        //增加60°
+        target_magz -= 1.0472/*（π/3）*/;
+        if (target_magz <= -3.141593/*（π）*/) {
+          target_magz += 3.141593 * 2;
+        }
+        counter = 250;
+      }
+    } else {
+      counter--;
+    }
+
+    //拨盘电机串级PID（开循环）
+    globals->pid_magz_position->SetCircular(true).SetCircularCycle(3.141593 * 2);
+    globals->pid_magz_position->Update(target_magz, globals->magazine_motor->pos(), 0.002);
+    // target_velocity = globals->pid_magz_position->out();
+    // globals->pid_magz_velocity->Update(target_velocity, globals->magazine_motor->vel(), 0.002);
+    //发送CAN
+    if (globals->rc->switch_r() == rm::device::DR16::SwitchPosition::kDown) {
+      globals->magazine_motor->SetPosition(0, 0, 0, 0, 0);
+    } else {
+      //
+      globals->magazine_motor->SetPosition(0, 0, globals->pid_magz_position->out(), 0, 0);
+    }
+  }
+  //失能
+  if (l_switch_position_last == rm::device::DR16::SwitchPosition::kMid && l_switch_position_now !=
+      rm::device::DR16::SwitchPosition::kMid) {
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+    globals->magazine_motor->SendInstruction(rm::device::DmMotorInstructions::kDisable);
+  }
+}
+/*----------------------------------------------------*/
+//摩擦轮逻辑
+void ShooterControl() {
+  //摩擦轮逻辑
+  if (globals->rc->switch_r() == rm::device::DR16::SwitchPosition::kDown) {
+    // 给摩擦轮电机发送指令
+    globals->pid_shooter_1->Update(0, globals->shooter_motor_1->rpm());
+    globals->pid_shooter_2->Update(0, globals->shooter_motor_2->rpm());
+    globals->pid_shooter_3->Update(0, globals->shooter_motor_3->rpm());
+    globals->pid_shooter_4->Update(0, globals->shooter_motor_4->rpm());
+    globals->pid_shooter_5->Update(0, globals->shooter_motor_5->rpm());
+    globals->pid_shooter_6->Update(0, globals->shooter_motor_6->rpm());
+  } else {
+    // 目标速度PID
+    globals->pid_shooter_1->Update(V_shooter_1, globals->shooter_motor_1->rpm());
+    globals->pid_shooter_2->Update(V_shooter_1, globals->shooter_motor_2->rpm());
+    globals->pid_shooter_3->Update(V_shooter_1, globals->shooter_motor_3->rpm());
+    globals->pid_shooter_4->Update(V_shooter_2, globals->shooter_motor_4->rpm());
+    globals->pid_shooter_5->Update(V_shooter_2, globals->shooter_motor_5->rpm());
+    globals->pid_shooter_6->Update(V_shooter_2, globals->shooter_motor_6->rpm());
+
+    // 给shooter电机发送指令
+    globals->shooter_motor_1->SetCurrent(static_cast<int16_t>(globals->pid_shooter_1->out()));
+    globals->shooter_motor_2->SetCurrent(static_cast<int16_t>(globals->pid_shooter_2->out()));
+    globals->shooter_motor_3->SetCurrent(static_cast<int16_t>(globals->pid_shooter_3->out()));
+    globals->shooter_motor_4->SetCurrent(static_cast<int16_t>(globals->pid_shooter_4->out()));
+    globals->shooter_motor_5->SetCurrent(static_cast<int16_t>(globals->pid_shooter_5->out()));
+    globals->shooter_motor_6->SetCurrent(static_cast<int16_t>(globals->pid_shooter_6->out()));
+  }
+  rm::device::DjiMotor<>::SendCommand();
+}
+/*----------------------------------------------------*/
+void ChassisControl() {
+  // 遥控器输入底盘速度
+  Vx = globals->rc->left_x() * 15000 / 660;
+  Vy = globals->rc->left_y() * 15000 / 660;
+  // Vw = globals->rc->dial() * 30000 / 660;
+
+  // 移动逻辑
+  rm::i16 V_wheel_1 = -Vy - Vx;
+  rm::i16 V_wheel_2 = Vy - Vx;
+  rm::i16 V_wheel_3 = Vy - 0.5 * Vx;
+  rm::i16 V_wheel_4 = -Vy - 0.5 * Vx;
+  if (globals->rc->switch_r() == rm::device::DR16::SwitchPosition::kDown) {
+    // 给底盘电机发送指令
+    globals->pid_chassis_1->Update(0, globals->chassis_motor_1->rpm());
+    globals->pid_chassis_2->Update(0, globals->chassis_motor_2->rpm());
+    globals->pid_chassis_3->Update(0, globals->chassis_motor_3->rpm());
+    globals->pid_chassis_4->Update(0, globals->chassis_motor_4->rpm());
+  } else {
+    // 目标速度PID
+    globals->pid_chassis_1->Update(V_wheel_1, globals->chassis_motor_1->rpm());
+    globals->pid_chassis_2->Update(V_wheel_2, globals->chassis_motor_2->rpm());
+    globals->pid_chassis_3->Update(V_wheel_3, globals->chassis_motor_3->rpm());
+    globals->pid_chassis_4->Update(V_wheel_4, globals->chassis_motor_4->rpm());
+
+    // 给chassis电机发送指令
+    globals->chassis_motor_1->SetCurrent(static_cast<int16_t>(globals->pid_chassis_1->out()));
+    globals->chassis_motor_2->SetCurrent(static_cast<int16_t>(globals->pid_chassis_2->out()));
+    globals->chassis_motor_3->SetCurrent(static_cast<int16_t>(globals->pid_chassis_3->out()));
+    globals->chassis_motor_4->SetCurrent(static_cast<int16_t>(globals->pid_chassis_4->out()));
+  }
+  rm::device::DjiMotor<>::SendCommand();
+}
--- app/targets/meca_hero/main.hpp
+++ app/targets/meca_hero/main.hpp
@@ -1,7 +1,3 @@
-//
-// Created by CHIPS KILLER on 2026/1/13.
-//
-
 #ifndef BOARDC_MAIN_HPP
 #define BOARDC_MAIN_HPP
 
@@ -11,78 +7,124 @@
 #include "spi.h"
 #include "timer_task.hpp"
 #include "tim.h"
+/*-------------------------------------------------
+ *变量
+ */
+inline struct GlobalWarehouse {
+  // 硬件接口 //
+  rm::hal::Can *can1{nullptr}, *can2{nullptr}; ///< CAN 总线接口
+  rm::hal::Serial *dbus{nullptr}; ///< 遥控器串口接口
 
-// //底盘速度
-//   rm::i16 Vx,Vy,Vw;
-//
-// struct GlobalWarehouse {
-//
-//   // 硬件接口 //
-//   rm::hal::Can *can1{nullptr}, *can2{nullptr};  ///< CAN 总线接口
-//   rm::hal::Serial *dbus{nullptr};               ///< 遥控器串口接口
-//
-//   // 设备 //
-//   rm::device::DR16 *rc{nullptr};           ///< 遥控器
-//   rm::device::GM6020 *yaw_motor{nullptr};  ///< 云台 Yaw 电机
-//   rm::device::DmMotor<rm::device::DmMotorControlMode::kMit> *magazine_motor{nullptr};  ///< 云台 Pitch 电机
-//   rm::device::BMI088 *imu{nullptr};///< BMI088 IMU
-//
-//   //创建电机对象
-//   rm::device::M3508 *wheel_motor_1{nullptr};
-//   rm::device::M3508 *wheel_motor_2{nullptr};
-//   rm::device::M3508 *wheel_motor_3{nullptr};
-//   rm::device::M3508 *wheel_motor_4{nullptr};
-//
-//   //创建PID控制器
-//   rm::modules::PID *pid_1{nullptr};
-//   rm::modules::PID *pid_2{nullptr};
-//   rm::modules::PID *pid_3{nullptr};
-//   rm::modules::PID *pid_4{nullptr};
-//
-//   // 控制器 //
-//   rm::modules::MahonyAhrs ahrs{1000.f};  ///< mahony 姿态解算器，频率 1000Hz
-//
-//   void Init() {
-//
-//     can1 = new rm::hal::Can{hcan1};
-//     can2 = new rm::hal::Can{hcan2};
-//     dbus = new rm::hal::Serial{huart3, 36, rm::hal::stm32::UartMode::kNormal, rm::hal::stm32::UartMode::kDma};
-//     //遥控
-//     rc = new rm::device::DR16{*dbus};//设置了遥控器以及用了串口
-//     //电机
-//     wheel_motor_1=new rm::device::M3508{*can1,1,false};
-//     wheel_motor_2=new rm::device::M3508{*can1,2,false};
-//     wheel_motor_3=new rm::device::M3508{*can1,3,false};
-//     wheel_motor_4=new rm::device::M3508{*can1,4,false};
-//     // yaw_motor = new rm::device::GM6020{*can1, 1};
-//     // magazine_motor = new rm::device::DmMotor<rm::device::DmMotorControlMode::kMit>{*can2, {}};
-//     // imu = new rm::device::BMI088{hspi1, CS1_ACCEL_GPIO_Port, CS1_ACCEL_Pin, CS1_GYRO_GPIO_Port, CS1_GYRO_Pin};
-//     //PID控制器
-//     pid_1=new rm::modules::PID{10,2,2,30000,2};
-//     pid_2=new rm::modules::PID{10,2,2,30000,2};
-//     pid_3=new rm::modules::PID{10,2,2,30000,2};
-//     pid_4=new rm::modules::PID{10,2,2,30000,2};
-//
-//
-//
-//     can1->SetFilter(0, 0);
-//     can1->Begin();
-//     can2->SetFilter(0, 0);
-//     can2->Begin();
-//     rc->Begin();  // 启动遥控器接收，这行或许比较适合放到AppMain里面？
-//   }
-// } *globals;
+  // 设备 //
+  rm::device::DR16 *rc{nullptr}; ///< 遥控器
+  // rm::device::GM6020 *yaw_motor{nullptr};                                              ///< 云台 Yaw 电机
+  // rm::device::DmMotor<rm::device::DmMotorControlMode::kMit> *magazine_motor{nullptr};  ///< 云台 Pitch 电机
+  rm::device::BMI088 *imu{nullptr}; ///< BMI088 IMU
 
-// //创建电机对象
-// rm::device::M3508 wheel_motor_1(*(globals->can1),1,false);
-// rm::device::M3508 wheel_motor_2(*(globals->can1),2,false);
-// rm::device::M3508 wheel_motor_3(*(globals->can1),3,false);
-// rm::device::M3508 wheel_motor_4(*(globals->can1),4,false);
+  // 创建电机对象
+  rm::device::M3508 *chassis_motor_1{nullptr};
+  rm::device::M3508 *chassis_motor_2{nullptr};
+  rm::device::M3508 *chassis_motor_3{nullptr};
+  rm::device::M3508 *chassis_motor_4{nullptr};
 
-// 创建PID控制器
-//  rm::modules::PID pid_1(5,0,0,30000,0);
-//  rm::modules::PID pid_2(5,0,0,30000,0);
-//  rm::modules::PID pid_3(5,0,0,30000,0);
-//  rm::modules::PID pid_4(5,0,0,30000,0);
+  rm::device::M3508 *shooter_motor_1{nullptr};
+  rm::device::M3508 *shooter_motor_2{nullptr};
+  rm::device::M3508 *shooter_motor_3{nullptr};
+  rm::device::M3508 *shooter_motor_4{nullptr};
+  rm::device::M3508 *shooter_motor_5{nullptr};
+  rm::device::M3508 *shooter_motor_6{nullptr};
 
+  rm::device::DmMotor<rm::device::DmMotorControlMode::kMit> *magazine_motor{nullptr};
+
+  // 创建PID控制器
+  rm::modules::PID *pid_chassis_1{nullptr};
+  rm::modules::PID *pid_chassis_2{nullptr};
+  rm::modules::PID *pid_chassis_3{nullptr};
+  rm::modules::PID *pid_chassis_4{nullptr};
+
+  rm::modules::PID *pid_shooter_1{nullptr};
+  rm::modules::PID *pid_shooter_2{nullptr};
+  rm::modules::PID *pid_shooter_3{nullptr};
+  rm::modules::PID *pid_shooter_4{nullptr};
+  rm::modules::PID *pid_shooter_5{nullptr};
+  rm::modules::PID *pid_shooter_6{nullptr};
+
+  rm::modules::PID *pid_magz_position{nullptr};
+  rm::modules::PID *pid_magz_velocity{nullptr};
+
+  // 控制器 //
+  rm::modules::MahonyAhrs ahrs{1000.f}; ///< mahony 姿态解算器，频率 1000Hz
+
+  void Init() {
+    can1 = new rm::hal::Can{hcan1};
+    can2 = new rm::hal::Can{hcan2};
+    dbus = new rm::hal::Serial{huart3, 36, rm::hal::stm32::UartMode::kNormal, rm::hal::stm32::UartMode::kDma};
+    // 遥控
+    rc = new rm::device::DR16{*dbus}; // 设置了遥控器以及用了串口
+    // 电机
+    chassis_motor_1 = new rm::device::M3508{*can2, 1, false};
+    chassis_motor_2 = new rm::device::M3508{*can2, 2, false};
+    chassis_motor_3 = new rm::device::M3508{*can2, 3, false};
+    chassis_motor_4 = new rm::device::M3508{*can2, 4, false};
+
+    shooter_motor_1 = new rm::device::M3508{*can1, 1, false};
+    shooter_motor_2 = new rm::device::M3508{*can1, 2, false};
+    shooter_motor_3 = new rm::device::M3508{*can1, 3, false};
+    shooter_motor_4 = new rm::device::M3508{*can1, 4, false};
+    shooter_motor_5 = new rm::device::M3508{*can1, 5, false};
+    shooter_motor_6 = new rm::device::M3508{*can1, 6, false};
+
+    magazine_motor = new rm::device::DmMotor<rm::device::DmMotorControlMode::kMit>{
+        *can1, {0x12, 0x03, 3.141593f, 30.0f, 10.0f, {0.0f, 500.0f}, {0.0f, 5.0f}}};
+
+    // PID控制器
+    pid_chassis_1 = new rm::modules::PID{20, 2, 4, 18000, 2};
+    pid_chassis_2 = new rm::modules::PID{20, 2, 4, 18000, 2};
+    pid_chassis_3 = new rm::modules::PID{25, 2, 4, 18000, 2};
+    pid_chassis_4 = new rm::modules::PID{25, 2, 4, 18000, 2};
+
+    pid_shooter_1 = new rm::modules::PID{25, 2, 4, 18000, 2}; //20
+    pid_shooter_2 = new rm::modules::PID{25, 2, 4, 18000, 2}; //20
+    pid_shooter_3 = new rm::modules::PID{25, 2, 4, 18000, 2};
+    pid_shooter_4 = new rm::modules::PID{25, 2, 4, 18000, 2};
+    pid_shooter_5 = new rm::modules::PID{25, 2, 4, 18000, 2};
+    pid_shooter_6 = new rm::modules::PID{25, 2, 4, 18000, 2};
+
+    pid_magz_position = new rm::modules::PID{2.25, 0.001, 0.149, 6, 0};
+    // pid_magz_velocity = new rm::modules::PID{0.17, 0, 0.0002, 6.4, 0};
+
+    can1->SetFilter(0, 0);
+    can1->Begin();
+    can2->SetFilter(0, 0);
+    can2->Begin();
+    rc->Begin(); // 启动遥控器接收，这行或许比较适合放到AppMain里面？
+  }
+} *globals;;
+
+// 底盘速度
+inline rm::i16 Vx, Vy, Vw;
+//拨盘增加角度
+inline float target_magz;
+inline float target_velocity;
+//左摇杆状态
+inline rm::device::DR16::SwitchPosition l_switch_position_now = rm::device::DR16::SwitchPosition::kUnknown;
+inline rm::device::DR16::SwitchPosition l_switch_position_last = rm::device::DR16::SwitchPosition::kUnknown;
+//拨盘反馈值
+inline float pos;
+inline float vel;
+//扳机计数
+inline int counter = 0;
+//摩擦轮速度
+inline rm::i16 V_shooter_1=-6000;
+inline rm::i16 V_shooter_2=-5500;
+
+/*----------------------------------------------
+ *执行函数
+ */
+//拨盘电机逻辑
+void MagazineControl();
+//摩擦轮电机逻辑
+void ShooterControl();
+//底盘逻辑
+void ChassisControl();
 #endif  // BOARDC_MAIN_HPP
