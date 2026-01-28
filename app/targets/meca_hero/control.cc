e94f9dfbe6a52e54b17429627c2d24cca594983b
chipskiller <201592842+chipskiller@users.noreply.github.com>
2026-01-28 05:32:50 +0000

    Fix code style issues with clang-format

--- app/targets/meca_hero/control.cc
+++ app/targets/meca_hero/control.cc
@@ -12,9 +12,9 @@ void MagazineControl() {
   }
   // 启动
   if (l_switch_position_last == rm::device::DR16::SwitchPosition::kDown) {
-    target_magz=globals->magazine_motor->pos();
+    target_magz = globals->magazine_motor->pos();
     // 检测当前角度，防止大幅转动
-    while (next_target_magz< globals->magazine_motor->pos()) {
+    while (next_target_magz < globals->magazine_motor->pos()) {
       next_target_magz += 1.0472 /*（π/3）*/;
     }
     while (next_target_magz > globals->magazine_motor->pos()) {
@@ -48,7 +48,7 @@ void MagazineControl() {
   // if (rm::modules::Wrap(target_magz - globals->magazine_motor->pos(), -3.141593, 3.141593) < -3.141593/18) {
   //   target_magz = globals->magazine_motor->pos() +3.141593 / 90;
   // }
-  //堵转检测
+  // 堵转检测
   // if (rm::modules::Wrap(target_magz - globals->magazine_motor->pos(), -3.141593, 3.141593) < -(3.141593 / 36)) {
   //   magz_compensation_count++;
   //   if (magz_compensation_count == 500) {
@@ -66,8 +66,8 @@ void MagazineControl() {
       if (rm::modules::Wrap(target_magz - globals->magazine_motor->pos(), -3.141593, 3.141593) < -3.141593 / 18) {
         target_magz = globals->magazine_motor->pos() + 3.141593 / 90;
       } else {
-        target_magz=next_target_magz;
-        next_target_magz-=3.141593/3;
+        target_magz = next_target_magz;
+        next_target_magz -= 3.141593 / 3;
         if (next_target_magz < -3.141593) {
           next_target_magz += 2 * 3.141593;
         }
@@ -178,7 +178,7 @@ void ChassisControl() {
   if (globals->rc->switch_l() == rm::device::DR16::SwitchPosition::kUp) {
     globals->pid_chassis_follow->SetCircular(true).SetCircularCycle(3.141593 * 2);
     globals->pid_chassis_follow->Update(1.5708, globals->gimbal_motor_yaw->pos(),
-                                        0.001); // 云台正位为电机编码器的-90°
+                                        0.001);  // 云台正位为电机编码器的-90°
     Vw = globals->pid_chassis_follow->out();
   } else {
     Vw = 0;
@@ -212,10 +212,9 @@ void ChassisControl() {
 void GimbalControl() {
   // IMU解算
   globals->imu->Update();
-  globals->ahrs.Update(rm::modules::ImuData6Dof{globals->imu->gyro_x(), globals->imu->gyro_y(),
-                                                globals->imu->gyro_z()+static_cast<float>(0.000088),
-                                                globals->imu->accel_x(), globals->imu->accel_y(),
-                                                globals->imu->accel_z()});
+  globals->ahrs.Update(rm::modules::ImuData6Dof{
+      globals->imu->gyro_x(), globals->imu->gyro_y(), globals->imu->gyro_z() + static_cast<float>(0.000088),
+      globals->imu->accel_x(), globals->imu->accel_y(), globals->imu->accel_z()});
   eulerangle_yaw = -globals->ahrs.euler_angle().yaw;
   eulerangle_pitch = -globals->ahrs.euler_angle().pitch;
   eulerangle_roll = -globals->ahrs.euler_angle().roll;
@@ -233,7 +232,6 @@ void GimbalControl() {
     return;
   }
 
-
   // 云台电机逻辑
 
   // 启动云台
@@ -243,7 +241,7 @@ void GimbalControl() {
   }
 
   // 遥控器输入云台角度
-  target_pos_yaw += static_cast<float>(globals->rc->right_x()) * 0.00001; //
+  target_pos_yaw += static_cast<float>(globals->rc->right_x()) * 0.00001;  //
   target_pos_pitch += static_cast<float>(globals->rc->right_y()) * 0.0000005;
   // yaw限位
   // if (target_pos_yaw < -1.85) {
@@ -275,7 +273,7 @@ void GimbalControl() {
 
   // yawPID计算（双环）
   globals->pid_yaw_position->SetCircular(true).SetCircularCycle(3.141593 * 2);
-  globals->pid_yaw_position->Update(target_pos_yaw, -globals->ahrs.euler_angle().yaw-0.005, 0.001);
+  globals->pid_yaw_position->Update(target_pos_yaw, -globals->ahrs.euler_angle().yaw - 0.005, 0.001);
   globals->pid_yaw_velocity->Update(globals->pid_yaw_velocity->out(), -globals->imu->gyro_z(), 0.002);
   // pitchPID计算
   globals->pid_pitch_position->Update(target_pos_pitch, -globals->ahrs.euler_angle().pitch, 1);
