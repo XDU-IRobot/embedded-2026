75c764654476a6f6ff1ea1568395586372a34c47
chenmixuexi <chenmixuexi5983@qq.com>
2026-03-21 20:24:31 +0800

    fix:修复档位问题。

--- app/targets/steer_infantry_gb/main.cc
+++ app/targets/steer_infantry_gb/main.cc
@@ -146,6 +146,7 @@ void GlobalWarehouse::RCStateUpdate() {
       switch (globals->rc->switch_l()) {
         case rm::device::DR16::SwitchPosition::kUp:
           globals->Music();
+          globals->StateMachine_ = kNoForce;  // 左拨杆拨到下侧，进入比赛模式，此时全部系统都上电工作
           break;
         case rm::device::DR16::SwitchPosition::kMid:
         case rm::device::DR16::SwitchPosition::kDown:
