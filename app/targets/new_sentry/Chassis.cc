d87af6ead28ec3ff48998eb94e132c55bc8d3201
chenmixuexi <chenmixuexi5983@qq.com>
2026-03-21 17:45:02 +0800

    fix:修复底盘小陀螺平移偏转问题。

--- app/targets/new_sentry/Chassis.cc
+++ app/targets/new_sentry/Chassis.cc
@@ -126,13 +126,15 @@ void Chassis::ChassisNavigateDataUpdate() {
                        -chassis->chassis_max_navigate_xyw_, chassis->chassis_max_navigate_xyw_,  //
                        -chassis->chassis_max_speed_w_, chassis->chassis_max_speed_w_);
   if (std::abs(chassis->chassis_target_w_) > 0) {
-    chassis->chassis_move_delta_angle_ *= chassis->chassis_target_w_;
+    f32 chassis_move_delta_angle =
+        chassis->chassis_move_delta_angle_ * rm::modules::Clamp(globals->navigate_communicator->chassis_target_w(),
+                                                                -chassis_max_navigate_xyw_, chassis_max_navigate_xyw_);
     chassis->chassis_target_x_ =
-        chassis->chassis_receive_x_ * std::cos(chassis->down_yaw_delta_ + chassis->chassis_move_delta_angle_) -
-        chassis->chassis_receive_y_ * std::sin(chassis->down_yaw_delta_ + chassis->chassis_move_delta_angle_);
+        chassis->chassis_receive_x_ * std::cos(chassis->down_yaw_delta_ + chassis_move_delta_angle) -
+        chassis->chassis_receive_y_ * std::sin(chassis->down_yaw_delta_ + chassis_move_delta_angle);
     chassis->chassis_target_y_ =
-        chassis->chassis_receive_y_ * std::cos(chassis->down_yaw_delta_ + chassis->chassis_move_delta_angle_) +
-        chassis->chassis_receive_x_ * std::sin(chassis->down_yaw_delta_ + chassis->chassis_move_delta_angle_);
+        chassis->chassis_receive_y_ * std::cos(chassis->down_yaw_delta_ + chassis_move_delta_angle) +
+        chassis->chassis_receive_x_ * std::sin(chassis->down_yaw_delta_ + chassis_move_delta_angle);
   } else {
     chassis->chassis_target_x_ = chassis->chassis_receive_x_ * std::cos(chassis->down_yaw_delta_) -
                                  chassis->chassis_receive_y_ * std::sin(chassis->down_yaw_delta_);
--- app/targets/new_sentry/Chassis.hpp
+++ app/targets/new_sentry/Chassis.hpp
@@ -31,9 +31,7 @@ inline class Chassis {
 
   f32 k_speed_power_limit_ = 1.0f;  // 底盘速度限制系数
 
-  f32 chassis_move_delta_angle_ = -0.15f;
-
-  bool rotate_flag_ = false;  // 小陀螺模式标识位
+  const f32 chassis_move_delta_angle_ = -0.15f;
 
   const f32 front_down_yaw_angle_ = -2.06f;  // 前方下部yaw轴角度
 
