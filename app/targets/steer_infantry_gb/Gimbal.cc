e5a5c90353fbfd039f84a644e0b6d5b23ea49b42
chenmixuexi <chenmixuexi5983@qq.com>
2026-03-23 06:56:56 +0800

    update

--- app/targets/steer_infantry_gb/Gimbal.cc
+++ app/targets/steer_infantry_gb/Gimbal.cc
@@ -1,5 +1,7 @@
 #include "Gimbal.hpp"
 
+f32 a, b;
+
 void Gimbal::GimbalInit() {
   gimbal->gimbal_yaw_target_ = globals->ahrs.euler_angle().yaw;
   gimbal->gimbal_pitch_target_ = -globals->ahrs.euler_angle().pitch;
@@ -151,8 +153,10 @@ void Gimbal::ShootEnableUpdate() {
   globals->shoot_controller.Arm(true);
   globals->shoot_controller.SetArmSpeed(gimbal->ammo_speed_ - static_cast<f32>(globals->aim_speed_change) * 100.0f);
   globals->dail_encoder_counter.Update(globals->dial_motor->encoder());
+  a = globals->chassis_communicator->heat_real();
+  b = globals->chassis_communicator->heat_limit();
   if (const u16 heat_delta = globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real();
-      globals->rc->dial() <= -650 || (heat_delta > 100 && (globals->df_state || globals->xf_state) &&
+      globals->rc->dial() <= -650 || (heat_delta > 30 && (globals->df_state || globals->xf_state) &&
                                       (globals->image_update_flag ? globals->image_data->data().mouse_button_left
                                                                   : globals->rc->mouse_button_left()))) {
     if (!gimbal->single_shoot_flag_) {
@@ -176,7 +180,7 @@ void Gimbal::ShootEnableUpdate() {
     globals->shoot_controller.SetMode(Shoot3Fric::kFullAuto);
     if (heat_delta > 100) {
       globals->shoot_controller.SetShootFrequency(20.0f);
-    } else if (heat_delta < 40) {
+    } else if (heat_delta < 20) {
       globals->shoot_controller.SetShootFrequency(0.0f);
     } else {
       globals->shoot_controller.SetShootFrequency(std::pow(static_cast<f32>(heat_delta) / 100.0f, 2.0f) * 20.0f);
