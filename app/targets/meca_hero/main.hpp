d68fe7a366517ff91e6a67eef9e60734e602af63
Qijun Wang <2100921316@qq.com>
2026-03-26 17:38:34 +0800

    弹速12m

--- app/targets/meca_hero/main.hpp
+++ app/targets/meca_hero/main.hpp
@@ -201,8 +201,8 @@ inline float vel;
 // 扳机计数
 inline int counter = 0;
 // 摩擦轮速度
-inline rm::i16 V_shooter_1 = -4500;
-inline rm::i16 V_shooter_2 = -3700;  // 12m/s
+inline rm::i16 V_shooter_1 = -4350;
+inline rm::i16 V_shooter_2 = -3600;  // 12m/s
 inline rm::i16 e_area = 100;
 inline rm::i16 limit = -3000;
 // 摩擦轮速度监测
