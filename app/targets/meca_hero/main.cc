2d1e18e999a93fb7809b7f3bc10bff8a61430f33
chipskiller <201592842+chipskiller@users.noreply.github.com>
2026-03-26 08:14:09 +0000

    Fix code style issues with clang-format

--- app/targets/meca_hero/main.cc
+++ app/targets/meca_hero/main.cc
@@ -170,10 +170,10 @@ extern "C" [[noreturn]] void AppMain(void) {
   // 创建主循环定时任务，定频1khz
   TimerTask mainloop_1000hz{
       &htim13,
-      etl::delegate<void()>::create<MainLoop>() //
+      etl::delegate<void()>::create<MainLoop>()  //
   };
-  mainloop_1000hz.SetPrescalerAndPeriod(100 - 1, 1000 - 1); // 84MHz / 100 / 1000 = 840Hz
-  mainloop_1000hz.Start(); // 启动定时器
+  mainloop_1000hz.SetPrescalerAndPeriod(100 - 1, 1000 - 1);  // 84MHz / 100 / 1000 = 840Hz
+  mainloop_1000hz.Start();                                   // 启动定时器
   globals->gyro_z_filter.set_cutoff_frequency(1000.0f, 50.0f);
 
   // ui_self_id = globals->ref.data().robot_status.robot_id;
