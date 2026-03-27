c67378c7449b9b4783a805e7ca430a331fce9618
chenmixuexi <chenmixuexi5983@qq.com>
2026-03-27 13:43:11 +0800

    update:修改热量闭环逻辑。

--- app/targets/steer_infantry_gb/Gimbal.cc
+++ app/targets/steer_infantry_gb/Gimbal.cc
@@ -161,11 +161,11 @@ void Gimbal::ShootEnableUpdate() {
   globals->shoot_controller.Arm(true);
   globals->shoot_controller.SetArmSpeed(gimbal->ammo_speed_ - static_cast<f32>(globals->aim_speed_change) * 100.0f);
   globals->dail_encoder_counter.Update(globals->dial_motor->encoder());
-  if (const i16 heat_delta =
-          static_cast<i16>(globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real());
-      globals->rc->dial() <= -650 || (heat_delta > 30 && (globals->df_state || globals->xf_state) &&
-                                      (globals->image_update_flag ? globals->image_data->data().mouse_button_left
-                                                                  : globals->rc->mouse_button_left()))) {
+  if (globals->rc->dial() <= -650 ||
+      (globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real() > 30 &&
+       (globals->df_state || globals->xf_state) &&
+       (globals->image_update_flag ? globals->image_data->data().mouse_button_left
+                                   : globals->rc->mouse_button_left()))) {
     if (!gimbal->single_shoot_flag_) {
       globals->shoot_controller.SetMode(Shoot3Fric::kSingleShot);
       gimbal->single_shoot_flag_ = true;
@@ -186,12 +186,14 @@ void Gimbal::ShootEnableUpdate() {
                 globals->aimbot_communicator->aimbot_state() >> 0 & 0x01 &&
                 globals->aimbot_communicator->aimbot_state() >> 1 & 0x01)))) {
     globals->shoot_controller.SetMode(Shoot3Fric::kFullAuto);
-    if (heat_delta > 60) {
+    if (globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real() > 60) {
       globals->shoot_controller.SetShootFrequency(20.0f);
-    } else if (heat_delta < 20) {
+    } else if (globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real() < 20) {
       globals->shoot_controller.SetShootFrequency(0.0f);
     } else {
-      globals->shoot_controller.SetShootFrequency(static_cast<f32>(heat_delta) / 3.0f);
+      globals->shoot_controller.SetShootFrequency(
+          static_cast<f32>(globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real()) /
+          3.0f);
     }
   } else {
     globals->shoot_controller.SetMode(Shoot3Fric::kStop);
