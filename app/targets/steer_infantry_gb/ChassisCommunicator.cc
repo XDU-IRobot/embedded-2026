9e7161357cab3108f500691237b9d1275c4c04b0
chenmixuexi <chenmixuexi5983@qq.com>
2026-03-28 12:53:58 +0800

    update

--- app/targets/steer_infantry_gb/ChassisCommunicator.cc
+++ app/targets/steer_infantry_gb/ChassisCommunicator.cc
@@ -1,10 +1,10 @@
 #include "ChassisCommunicator.hpp"
 
 namespace rm::device {
-ChassisCommunicator::ChassisCommunicator(hal::CanInterface &can) : CanDevice(can, 0x220) {}
+ChassisCommunicator::ChassisCommunicator(hal::CanInterface &can) : CanDevice(can, 0x100) {}
 
 void ChassisCommunicator::RxCallback(const hal::CanFrame *msg) {
-  if (msg->rx_std_id == 0x220) {
+  if (msg->rx_std_id == 0x100) {
     ReportStatus(kOk);
     heat_real_ = static_cast<u16>(msg->data[0]) << 8 | static_cast<u16>(msg->data[1]);
     heat_limit_ = static_cast<u16>(msg->data[2]) << 8 | static_cast<u16>(msg->data[3]);
@@ -26,6 +26,6 @@ void ChassisCommunicator::SendChassisCommand(f32 chassis_move_x, f32 chassis_mov
   tx_buf_[4] = get_target_flag;
   tx_buf_[5] = suggest_fire_flag;
   tx_buf_[6] = aim_speed_change;
-  this->can_->Write(0x210, tx_buf_, 8);
+  this->can_->Write(0x120, tx_buf_, 8);
 }
 }  // namespace rm::device
