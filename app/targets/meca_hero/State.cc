39e05d5e54cc1bafc90cec19122ea30b6106c3e5
Qijun Wang <2100921316@qq.com>
2026-04-09 17:53:40 +0800

    feat:状态机类

---
+++ app/targets/meca_hero/State.cc
@@ -0,0 +1,76 @@
+#include "State.hpp"
+#include "librm.hpp"
+#include "main.hpp"
+
+/**
+ * @brief:
+ * @note:
+ **/
+void StateMachine::MainStateUpdate() {
+  VT03::SwitchPosition tc_switch_position = globals->tc->data().switch_position;
+  DR16::SwitchPosition rc_switch_position = globals->rc->switch_r();
+
+  last_main_state_ = current_main_state_;
+  //图传最高优先级
+  if (tc_switch_position == VT03::SwitchPosition::C) {
+    current_main_state_ = MainState::kOffline;
+    return;
+  } else if (tc_switch_position == VT03::SwitchPosition::S) {
+    current_main_state_ = MainState::kGame;
+    return;
+  }
+  //DT7控制模式
+  switch (rc_switch_position) {
+    case DR16::SwitchPosition::kUnknown:
+      current_main_state_ = MainState::kOffline;
+      break;
+
+    case DR16::SwitchPosition::kDown:
+      current_main_state_ = MainState::kOffline;
+      break;
+
+    case DR16::SwitchPosition::kMid:
+      switch (current_main_state_) {
+        case MainState::kOffline:
+          current_main_state_ = MainState::kWaiting;
+          break;
+        case MainState::kWaiting:
+          if (count > 0) {
+            count--;
+          } else {
+            current_main_state_ = MainState::kTest;
+            count = waiting_count_;
+            break;
+          }
+        default:
+          current_main_state_ = MainState::kTest;
+          break;
+      }
+      break;
+
+    case DR16::SwitchPosition::kUp:
+      switch (current_main_state_) {
+        case MainState::kOffline:
+          current_main_state_ = MainState::kWaiting;
+          break;
+        case MainState::kTest:
+          current_main_state_ = MainState::kWaiting;
+          break;
+        case MainState::kWaiting:
+          if (count > 0) {
+            count--;
+          } else {
+            current_main_state_ = MainState::kGame;
+            count = waiting_count_;
+            break;
+          }
+        default:
+          current_main_state_ = MainState::kGame;
+          break;
+      }
+      break;
+    default:
+      current_main_state_ = MainState::kOffline;
+      break;
+  }
+}
--- app/targets/meca_hero/State.hpp
+++ app/targets/meca_hero/State.hpp
@@ -1,11 +1,44 @@
 #ifndef BOARDC_STATE_HPP
 #define BOARDC_STATE_HPP
-class State_Management {
-  State_Management()=default;
-  enum class main_state {
-    Offline,
-    Game,
+
+class StateMachine {
+public:
+  StateMachine() = delete;
+
+  explicit StateMachine(int waiting_count) : waiting_count_(waiting_count), count(waiting_count) {}
+
+  void SetWaitingCount(int count) { waiting_count_ = count; }
+
+  enum class MainState {
+    kOffline,
+    kGame,
+    kTest,
+    kWaiting,
   };
 
+  enum class SubState {
+    kNoAct,
+    kFollow,
+    kAimbot,
+    kSnipe,
+    kRadarAimbot,
+  };
+
+  void MainStateUpdate();
+
+  void SubStateUpdate();
+
+  [[nodiscard]] MainState getMainState() const { return current_main_state_; };
+
+  [[nodiscard]] SubState getSubState() const { return current_sub_state_; };
+
+private:
+  MainState current_main_state_{MainState::kOffline};
+  MainState last_main_state_{MainState::kOffline};
+  SubState current_sub_state_{SubState::kNoAct};
+  SubState last_sub_state_{SubState::kNoAct};
+
+  int waiting_count_{0};
+  int count {0};
 };
 #endif  // BOARDC_STATE_HPP
