3fcf1e96025be5d68be0f95c9007451ae1cacbc9
chipskiller <201592842+chipskiller@users.noreply.github.com>
2026-04-14 11:50:37 +0000

    Fix code style issues with clang-format

--- app/targets/meca_hero/State.hpp
+++ app/targets/meca_hero/State.hpp
@@ -46,7 +46,7 @@ class StateMachine {
 
   void StateUpdate();
   void DT7Switch();
-  void Waiting(){
+  void Waiting() {
     count_ = waiting_count_;  ////使能时间段
     if (count_ > 0) {
       count_--;
@@ -64,35 +64,43 @@ class StateMachine {
       } else {
         count = waiting_count_;
       }
-      }
-      if(current_chassis_state_ == ChassisState::kNormal) {last_chassis_state_ = current_chassis_state_;current_chassis_state_ = ChassisState::kFollow;}
-      else if (current_chassis_state_ == ChassisState::kFollow) {current_chassis_state_ = last_chassis_state_; last_chassis_state_ = ChassisState::kFollow;}
+    }
+    if (current_chassis_state_ == ChassisState::kNormal) {
+      last_chassis_state_ = current_chassis_state_;
+      current_chassis_state_ = ChassisState::kFollow;
+    } else if (current_chassis_state_ == ChassisState::kFollow) {
+      current_chassis_state_ = last_chassis_state_;
+      last_chassis_state_ = ChassisState::kFollow;
     }
   }
-  
+}
 
-  [[nodiscard]] MainState getMainState() const { return current_main_state_; };
-  [[nodiscard]] SubState getSubState() const { return current_sub_state_; };
-  [[nodiscard]] ChassisState getChassisState() const { return current_chassis_state_; };
-  [[nodiscard]] GimbalState getGimbalState() const { return current_gimbal_state_; };
-  [[nodiscard]] AmmoState getAmmoState() const { return current_ammo_state_; };
-
- private:
-  MainState current_main_state_{MainState::kOffline};
-  MainState last_main_state_{MainState::kOffline};
-
-  SubState current_sub_state_{SubState::kNormal};
-  SubState last_sub_state_{SubState::kNormal};
-
-  ChassisState current_chassis_state_{ChassisState::kOffline};
-  ChassisState last_chassis_state_{ChassisState::kNormal};
-
-  AmmoState current_ammo_state_{AmmoState::kNormal};
-  AmmoState last_ammo_state_{AmmoState::kNormal};
-
-  GimbalState current_gimbal_state_{GimbalState::kOffline};
-  GimbalState last_gimbal_state_{GimbalState::kNormal};
-
-  int waiting_count_{0};
-  int count_{0};
+[[nodiscard]] MainState
+getMainState() const {
+  return current_main_state_;
 };
+[[nodiscard]] SubState getSubState() const { return current_sub_state_; };
+[[nodiscard]] ChassisState getChassisState() const { return current_chassis_state_; };
+[[nodiscard]] GimbalState getGimbalState() const { return current_gimbal_state_; };
+[[nodiscard]] AmmoState getAmmoState() const { return current_ammo_state_; };
+
+private:
+MainState current_main_state_{MainState::kOffline};
+MainState last_main_state_{MainState::kOffline};
+
+SubState current_sub_state_{SubState::kNormal};
+SubState last_sub_state_{SubState::kNormal};
+
+ChassisState current_chassis_state_{ChassisState::kOffline};
+ChassisState last_chassis_state_{ChassisState::kNormal};
+
+AmmoState current_ammo_state_{AmmoState::kNormal};
+AmmoState last_ammo_state_{AmmoState::kNormal};
+
+GimbalState current_gimbal_state_{GimbalState::kOffline};
+GimbalState last_gimbal_state_{GimbalState::kNormal};
+
+int waiting_count_{0};
+int count_{0};
+}
+;
