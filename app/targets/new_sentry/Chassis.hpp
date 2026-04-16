8491859f512b02037998e5c97b6b3e1c80347551
chenmixuexi <chenmixuexi5983@qq.com>
2026-04-16 19:43:23 +0800

    update:优化功率。

--- app/targets/new_sentry/Chassis.hpp
+++ app/targets/new_sentry/Chassis.hpp
@@ -36,9 +36,9 @@ inline class Chassis {
 
   const f32 front_down_yaw_angle_ = 3.0f;  // 前方下部yaw轴角度
 
-  const f32 chassis_sensitivity_xy_ = 3000.0f;  // 底盘x、y轴灵敏度
-  const f32 chassis_max_speed_xy_ = 10000.0f;   // 底盘x、y轴最大速度
-  const f32 chassis_max_speed_w_ = 10000.0f;    // 底盘转动最大速度
+  const f32 chassis_sensitivity_xy_ = 4500.0f;  // 底盘x、y轴灵敏度
+  const f32 chassis_max_speed_xy_ = 20000.0f;   // 底盘x、y轴最大速度
+  const f32 chassis_max_speed_w_ = 20000.0f;    // 底盘转动最大速度
   const f32 chassis_max_navigate_xyw_ = 1.0f;   // 底盘x、y、z轴导航最速度
 
  public:
