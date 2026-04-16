b127f5c3e8a33dce3b6b5ae30a5b208c25f16523
chipskiller <201592842+chipskiller@users.noreply.github.com>
2026-04-16 11:25:30 +0000

    Fix code style issues with clang-format

--- app/targets/meca_hero/State.hpp
+++ app/targets/meca_hero/State.hpp
@@ -3,7 +3,6 @@
 // #include "main.hpp"
 #include "librm.hpp"
 
-
 class StateMachine {
  public:
   StateMachine() = delete;
@@ -99,18 +98,11 @@ class StateMachine {
    */
   void DialSwitch();
 
-
-
-  [[nodiscard]] MainState getMainState() const {
-    return current_main_state_; }
-  [[nodiscard]] SubState getSubState() const {
-    return current_sub_state_; }
-  [[nodiscard]] ChassisState getChassisState() const {
-    return current_chassis_state_; }
-  [[nodiscard]] GimbalState getGimbalState() const {
-    return current_gimbal_state_; }
-  [[nodiscard]] AmmoState getAmmoState() const {
-    return current_ammo_state_; }
+  [[nodiscard]] MainState getMainState() const { return current_main_state_; }
+  [[nodiscard]] SubState getSubState() const { return current_sub_state_; }
+  [[nodiscard]] ChassisState getChassisState() const { return current_chassis_state_; }
+  [[nodiscard]] GimbalState getGimbalState() const { return current_gimbal_state_; }
+  [[nodiscard]] AmmoState getAmmoState() const { return current_ammo_state_; }
 
  private:
   MainState current_main_state_{MainState::kOffline};
