f94603d839a0a1ce5b5e1703b70e611e506da431
Zyf_de_github <3397337213@qq.com>
2026-05-05 17:59:00 +0800

    航灯更新

---
+++ app/targets/drone_gb_new/WS2812b.hpp
@@ -0,0 +1,17 @@
+#ifndef BOARDC_WS2812B_HPP
+#define BOARDC_WS2812B_HPP
+
+#include "main.h"
+#include <math.h>
+
+#define MAX_LED 4
+#define USE_BRIGHTNESS 1
+
+// 设置LED颜色数据
+void Set_LED(int LEDnum, int Red, int Green, int Blue);
+// 设置LED亮度
+void Set_Brightness(int brightness);
+// 发送LED数据
+void WS2812_Send();
+
+#endif  // BOARDC_WS2812B_HPP
