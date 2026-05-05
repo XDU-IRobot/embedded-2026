03bbfa30dd1ebc4a16cfd6c22b8d330e2d182d89
Zhihao Gong <3554424129@qq.com>
2026-05-05 15:12:31 +0800

    改版参数

--- app/targets/drone_gb_new/gimbal.hpp
+++ app/targets/drone_gb_new/gimbal.hpp
@@ -61,7 +61,7 @@ class Gimbal {
 
   // pitch补偿系数
   float pitch_torque = 0.0f;     // pitch电机力矩重力补偿量
-  float pitch_torque_kp = 0.5f;  // TODO 重力补偿参数
+  float pitch_torque_kp = 0.35f;  // TODO 重力补偿参数
 
   float pitch_cmd = 0.0f;       // pitch合输出
   float pitch_speed_tf = 0.0f;  // 速度正向输出
@@ -184,10 +184,10 @@ class Gimbal {
     gimbal_controller.pid().yaw_speed.SetKp(350.0f).SetKi(0.0f).SetKd(0.0f).SetMaxOut(25000.0f).SetMaxIout(1000.0f);
     // yaw原始参数 200 0 0.2  350 0 0
 
-    gimbal_controller.pid().pitch_position.SetKp(35.0f).SetKi(0.002f).SetKd(0.01f).SetMaxOut(500.0f).SetMaxIout(
-        10.0f);  // TODO pitch初版参数 35
-    gimbal_controller.pid().pitch_speed.SetKp(0.8f).SetKi(0.0f).SetKd(0.001f).SetMaxOut(10.0f).SetMaxIout(5.0f);
-  }
+    gimbal_controller.pid().pitch_position.SetKp(20.0f).SetKi(0.001f).SetKd(0.001f).SetMaxOut(500.0f).SetMaxIout(
+        10.0f);  // TODO pitch初版参数 35 0 0.01
+    gimbal_controller.pid().pitch_speed.SetKp(1.2f).SetKi(0.0f).SetKd(0.003f).SetMaxOut(10.0f).SetMaxIout(5.0f);
+  }//0.8 0.0 0.001
 
   void AmmoPIDInit() {
     shoot_controller.pid().fric_1_speed.SetKp(18.0f).SetKi(0.0f).SetKd(0.0f).SetMaxOut(20000.0f).SetMaxIout(1000.0f);
