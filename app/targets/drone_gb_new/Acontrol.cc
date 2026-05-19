a22e339832acea7db343024c01d20dc92a65e9fa
liamao2 <147472436+liamao2@users.noreply.github.com>
2026-05-19 10:20:53 +0000

    Fix code style issues with clang-format

--- app/targets/drone_gb_new/Acontrol.cc
+++ app/targets/drone_gb_new/Acontrol.cc
@@ -66,8 +66,9 @@ void Gimbal::GimbalControl() {
 
     // 前馈计算项
     UpdateRcAngleDiff(roll_comp.first, roll_comp.second, 0.002f);
-    Eigen::Vector2f ff_torque = drone_gb.ComputeFf(-rm::modules::Wrap(yaw_motor->pos_rad() - 5.14, -M_PI, M_PI), -0.45 - pitch_motor->pos(), rc_yaw_vel, rc_pitch_vel,
-                                                   rc_yaw_acc, rc_pitch_acc, Eigen::Vector3f(0.0f, 0.0f, -9.81f));
+    Eigen::Vector2f ff_torque =
+        drone_gb.ComputeFf(-rm::modules::Wrap(yaw_motor->pos_rad() - 5.14, -M_PI, M_PI), -0.45 - pitch_motor->pos(),
+                           rc_yaw_vel, rc_pitch_vel, rc_yaw_acc, rc_pitch_acc, Eigen::Vector3f(0.0f, 0.0f, -9.81f));
     yaw_torque = ff_torque(0);
     pitch_torque = ff_torque(1);
 
@@ -138,9 +139,9 @@ void Gimbal::GimbalControl() {
 
     // 前馈计算项
     UpdateRcAngleDiff(roll_comp.first, roll_comp.second, 0.002f);
-    Eigen::Vector2f ff_torque =
-        drone_gb.ComputeFf(-rm::modules::Wrap(yaw_motor->pos_rad() - 5.14, -M_PI, M_PI), -0.45 - pitch_motor->pos(), rc_yaw_vel, rc_pitch_vel, rc_yaw_acc, rc_pitch_acc,
-                           Eigen::Vector3f(0.0f, 0.0f, -9.81f));  // TODO要改成自瞄给的值
+    Eigen::Vector2f ff_torque = drone_gb.ComputeFf(
+        -rm::modules::Wrap(yaw_motor->pos_rad() - 5.14, -M_PI, M_PI), -0.45 - pitch_motor->pos(), rc_yaw_vel,
+        rc_pitch_vel, rc_yaw_acc, rc_pitch_acc, Eigen::Vector3f(0.0f, 0.0f, -9.81f));  // TODO要改成自瞄给的值
     yaw_torque = ff_torque(0);
     pitch_torque = ff_torque(1);
 
