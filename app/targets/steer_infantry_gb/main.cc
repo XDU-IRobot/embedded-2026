21c717baa7c46700679257ce7354b4af5b0c2ddb
chenmixuexi <chenmixuexi5983@qq.com>
2026-05-21 12:59:16 +0800

    feat:优化can线。

--- app/targets/steer_infantry_gb/main.cc
+++ app/targets/steer_infantry_gb/main.cc
@@ -43,8 +43,8 @@ void GlobalWarehouse::Init() {
   buzzer = new Buzzer;
   led = new LED;
 
-  can1 = new rm::hal::ThrottledCan<128>{7000.0f, hcan1};
-  can2 = new rm::hal::ThrottledCan<128>{7000.0f, hcan2};
+  can1 = new rm::hal::ThrottledCan<128, rm::modules::SchedulingPolicy::kFifo>{7000.0f, hcan1};
+  can2 = new rm::hal::ThrottledCan<128, rm::modules::SchedulingPolicy::kFifo>{7000.0f, hcan2};
   aimbot_communicator = new rm::device::AimbotCanCommunicator{*can1};
   chassis_communicator = new rm::device::ChassisCommunicator{*can1};
   super_cap = new rm::device::GkSupercap{*can1};
