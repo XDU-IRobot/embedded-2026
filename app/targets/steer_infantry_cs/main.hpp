ab772dfc57c5343864543456c52aca986fd48784
chenmixuexi5983 <180763856+chenmixuexi5983@users.noreply.github.com>
2026-05-27 16:04:05 +0000

    Fix code style issues with clang-format

--- app/targets/steer_infantry_cs/main.hpp
+++ app/targets/steer_infantry_cs/main.hpp
@@ -42,8 +42,8 @@ inline struct GlobalWarehouse {
       led_controller;  ///< RGB LED控制器
 
   // 硬件接口 //
-  rm::hal::Can *can1{nullptr}, *can2{nullptr};  ///< CAN 总线接口
-  rm::device::GimbalCommunicator *gimbal_communicator{nullptr};                                     ///< CAN 通信器
+  rm::hal::Can *can1{nullptr}, *can2{nullptr};                                        ///< CAN 总线接口
+  rm::device::GimbalCommunicator *gimbal_communicator{nullptr};                       ///< CAN 通信器
   rm::device::GkSupercap *super_cap{nullptr};                                         ///< 港科超级电容
   rm::device::BMI088 *imu{nullptr};                                                   ///< IMU
   rm::hal::Serial<128> *referee_uart{nullptr};                                        ///< 裁判系统串口接口
