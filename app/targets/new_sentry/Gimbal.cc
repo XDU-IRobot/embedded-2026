4de4488bbda991f562e92d456c8881ce61c4a0f1
chenmixuexi <chenmixuexi5983@qq.com>
2026-05-28 08:53:31 +0800

    update

--- app/targets/new_sentry/Gimbal.cc
+++ app/targets/new_sentry/Gimbal.cc
@@ -425,8 +425,9 @@ void Gimbal::GimbalMovePIDUpdate() {
                                     globals->ahrs.euler_angle().yaw, globals->imu->gyro_z(),
                                     globals->hipnuc_imu->pitch(), -globals->hipnuc_imu->gyro_x(), 2.0f);
   const Eigen::Vector3f g_stationary(0.0f, 0.0f, -9.81f);
-  const auto ff = g_gimbal_dynamics.ComputeFf(globals->up_yaw_motor->encoder(), -globals->pitch_motor->pos() - 2.2f, 0,
-                                              0, 0, 0, g_stationary);
+  const auto ff = g_gimbal_dynamics.ComputeFf(globals->up_yaw_motor->encoder(), -globals->pitch_motor->pos() - 2.2f,
+                                              gimbal->yaw_speed_ref, gimbal->pitch_speed_ref, gimbal->yaw_accel_ref,
+                                              gimbal->pitch_accel_ref, g_stationary);
   gimbal->yaw_torque_ = ff.x();
   const f32 yaw_ff_voltage =
       YawTorqueToVoltageCmd(gimbal->yaw_torque_, static_cast<f32>(globals->up_yaw_motor->rpm()) * kRpmToRadPerSec);
@@ -451,7 +452,7 @@ void Gimbal::GimbalPIDUpdate() {
              (globals->aimbot_communicator->aimbot_state() >> 0 & 0x01 || gimbal->aimbot_time_ > 0)) {
     globals->gimbal_controller.EnableSpeedPid(true);
     globals->gimbal_controller.pid().up_yaw_position.SetKp(20.0f).SetKd(100.0f);
-    globals->gimbal_controller.pid().up_yaw_speed.SetKp(8800.0f).SetKd(0.0f);
+    globals->gimbal_controller.pid().up_yaw_speed.SetKp(8000.0f).SetKd(0.0f);
     globals->gimbal_controller.pid().pitch_position.SetKp(80.0f).SetKd(0.0f);
     globals->gimbal_controller.pid().pitch_speed.SetKp(0.5f).SetKd(0.0f);
   } else {
