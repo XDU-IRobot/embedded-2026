7557152abffe51c1e8fec16365dbbd08016779f7
Zhihao Gong <3554424129@qq.com>
2026-07-30 00:00:06 +0800

    fix:备车上场弹频修改

--- app/targets/steer_infantry_gb/Gimbal.cc
+++ app/targets/steer_infantry_gb/Gimbal.cc
@@ -229,7 +229,7 @@ void Gimbal::GimbalMovePIDUpdate() {
   gimbal->yaw_current_ =
       globals->gimbal_controller.output().yaw + static_cast<f32>(globals->yaw_motor->rpm()) * 100.f + yaw_ff_voltage;
   gimbal->yaw_current_ = rm::modules::Clamp(gimbal->yaw_current_, -kGm6020VoltageCmdLimit, kGm6020VoltageCmdLimit);
-  gimbal->pitch_torque_ = globals->gimbal_controller.output().pitch + ff.y()+0.01;
+  gimbal->pitch_torque_ = globals->gimbal_controller.output().pitch + ff.y()-0.001;
   gimbal->pitch_torque_ = rm::modules::Clamp(gimbal->pitch_torque_, -10.f, 10.f);
 }
 
@@ -237,7 +237,7 @@ void Gimbal::ApplyNormalGimbalPID() {
   globals->gimbal_controller.pid().yaw_position.SetKp(380.0f).SetKi(0).SetKd(9000.0f).SetMaxOut(30000.0f).SetMaxIout(0);
   globals->gimbal_controller.pid().yaw_speed.SetKp(580.0f).SetKi(0).SetKd(0.0f).SetMaxOut(30000.0f).SetMaxIout(0);
   // pitch PID 参数
-  globals->gimbal_controller.pid().pitch_position.SetKp(42.0f).SetKi(0).SetKd(600.0f).SetMaxOut(10000.0f).SetMaxIout(0);
+  globals->gimbal_controller.pid().pitch_position.SetKp(50.0f).SetKi(0).SetKd(600.0f).SetMaxOut(10000.0f).SetMaxIout(0);
   globals->gimbal_controller.pid().pitch_speed.SetKp(0.5f).SetKi(0).SetKd(0.0f).SetMaxOut(10.0f).SetMaxIout(0);
 }
 
@@ -419,7 +419,7 @@ void Gimbal::ShootEnableUpdate() {
                  globals->aimbot_communicator->aimbot_state() >> 1 & 0x01)))) {
     globals->shoot_controller.SetMode(Shoot3Fric::kFullAuto);
     if (globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real() > 100) {
-      globals->shoot_controller.SetShootFrequency(10.0f);//自瞄测试弹频修改至10（原来是20）
+      globals->shoot_controller.SetShootFrequency(20.0f);
     } else if (globals->chassis_communicator->heat_limit() - globals->chassis_communicator->heat_real() < 30) {
       globals->shoot_controller.SetShootFrequency(0.0f);
     } else {
